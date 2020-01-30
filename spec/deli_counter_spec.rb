078ca0650531052829d6f442793f2f2da58e5084
richard dinh <richardndinh@gmail.com>
2020-01-30 05:31:48 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -74,3 +74,29 @@ describe 'Deli Counter' do
   end
 
 end
+
+def line(deli)
+  if deli.length == 0 
+    puts "The line is currently empty"
+  else
+    str "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      str += " #{i} #{person}"
+      puts "#{i}. #{person}"
+    end
+    puts str
+  end
+end
+ 
+
+def take_a_number(deli, person)
+  deli << person
+  puts "Welcome, #{person} You are number #{deli.index(person) + 1} in line."
+end
+
+def now_serving(deli)
+  if deli.length == 0 
+  puts "There is no body waiting to be served!"
+else 
+ puts "Currently serving #{deli, first}"
+end
